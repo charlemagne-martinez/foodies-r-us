403c2e9e717a0c2224db96c69a20ec9734112105
Bruce Yan <yanbr@flip2.engr.oregonstate.edu>
2024-02-15 14:36:30 -0800

    selects for dmq

---
+++ Group127DMQ.sql
@@ -0,0 +1,20 @@
+--SELECT ALL DATA TO SHOW ON UI
+
+--Select all users
+SELECT * FROM Users;
+
+--Select all restaurants
+SELECT * FROM Restaurants;
+
+--Select all reviews
+SELECT * FROM Reviews;
+
+--Select all cuisine types
+SELECT * FROM CuisineTypes;
+
+--Select all restaurant chains
+SELECT * FROM RestaurantChains;
+
+--Select all locations
+SELECT * FROM Locations;
+
