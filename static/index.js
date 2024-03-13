dc98071687bac58fadfb1d30d7cad20a37477933
Bruce Yan <yanbr@flip3.engr.oregonstate.edu>
2024-03-13 10:45:29 -0700

    user update modal

--- static/index.js
+++ static/index.js
@@ -13,7 +13,7 @@ function updateLocation(locationID, city, state, country) {
 
 }
 
-function hideSellSomethingModal() {
+function hideLocationgModal() {
     var showSomethingModal = document.getElementById('update-modal')
     var modalBackdrop = document.getElementById('modal-backdrop')
 
@@ -22,24 +22,54 @@ function hideSellSomethingModal() {
 
 }
 
-function handleModalAcceptClick() {
-    
-    //clear items
-    hideSellSomethingModal()
-    
+function updateUser(userID, first, last, username, email, password){
+    var showSomethingModal = document.getElementById('update-modal-user');
+    var modalBackdrop = document.getElementById('modal-backdrop-user');
+
+    showSomethingModal.classList.remove('hidden');
+    modalBackdrop.classList.remove('hidden');
+
+    document.getElementById("update-userID").value = userID
+    document.getElementById("update-fName").value = first
+    document.getElementById("update-lName").value = last
+    document.getElementById("update-username").value = username
+    document.getElementById("update-email").value = email
+    document.getElementById("update-password").value = password 
+}
+
+function hideUserModal() {
+    var showSomethingModal = document.getElementById('update-modal-user')
+    var modalBackdrop = document.getElementById('modal-backdrop-user')
+
+    showSomethingModal.classList.add('hidden')
+    modalBackdrop.classList.add('hidden')
+
 }
 
 
-window.addEventListener('DOMContentLoaded', function () {
 
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
     var modalAcceptButton = document.getElementById('modal-accept')
     if (modalAcceptButton) {
-        modalAcceptButton.addEventListener('click', handleModalAcceptClick)
+        modalAcceptButton.addEventListener('click', hideLocationgModal)
     }
 
     var modalHideButtons = document.getElementsByClassName('modal-hide-button')
     for (var i = 0; i < modalHideButtons.length; i++) {
-        modalHideButtons[i].addEventListener('click', hideSellSomethingModal)
+        modalHideButtons[i].addEventListener('click', hideLocationgModal)
+    }
+
+    var modalAcceptButton = document.getElementById('modal-accept')
+    if (modalAcceptButton) {
+        modalAcceptButton.addEventListener('click', hideUserModal)
+    }
+
+    var modalHideButtons = document.getElementsByClassName('modal-hide-button')
+    for (var i = 0; i < modalHideButtons.length; i++) {
+        modalHideButtons[i].addEventListener('click', hideUserModal)
     }
 
 })
